2377b2952ab4835d8ad68a42bd06f22ec01d0bcb
Remco Beckers <remco.beckers@gmail.com>
2015-08-07 08:40:44 +0200

    Cleanup macro

--- src/main/scala/com/stackstate/scalajavamapper/ConverterMacro.scala
+++ src/main/scala/com/stackstate/scalajavamapper/ConverterMacro.scala
@@ -5,22 +5,13 @@ import scala.reflect.macros.whitebox.Context
 object ConverterMacro {
   def converter[T: c.WeakTypeTag, J: c.WeakTypeTag](c: Context): c.Expr[Converter[T, J]] = {
     import c.universe._
-    val tpe = weakTypeOf[T]
-    val tpj = weakTypeOf[J]
+    val tpeCaseClass = weakTypeOf[T]
+    val tpeJavaClass = weakTypeOf[J]
 
-    val companion = tpe.typeSymbol.companion
-    val fields = tpe.decls.collectFirst {
-      case m: MethodSymbol if m.isPrimaryConstructor ⇒ m
-    }.get.paramLists.head
-
-    val (toJavaStatements, fromJavaParams) = fields.map { field ⇒
-      val name = field.asTerm.name
-      val decoded = name.decodedName.toString
-      val scalaFieldType = tpe.decl(name).typeSignature
-
-      val setterName = s"set${decoded.capitalize}"
-      val javaSetter = tpj.members.find(_.asTerm.name.decodedName.toString == setterName).getOrElse(
-        c.abort(c.enclosingPosition, s"Setter $setterName not found on ${tpj.typeSymbol.name}.")
+    def javaSetter(fieldName: String): (c.universe.TermName, c.universe.Type) = {
+      val setterName = s"set${fieldName.capitalize}"
+      val javaSetter = tpeJavaClass.members.find(_.asTerm.name.decodedName.toString == setterName).getOrElse(
+        c.abort(c.enclosingPosition, s"Setter $setterName not found on ${tpeJavaClass.typeSymbol.name}.")
       )
       val javaSetterName = javaSetter.asTerm.name
       val javaSetterParams = javaSetter.typeSignature.paramLists.head
@@ -28,15 +19,33 @@ object ConverterMacro {
         c.abort(c.enclosingPosition, s"Setter ${javaSetterName} requires exactly one parameter, found ${javaSetterParams.size}")
       }
       val javaSetterType = javaSetterParams.head.typeSignature
+      (javaSetterName, javaSetterType)
+    }
 
-      val getterName = s"get${decoded.capitalize}"
-      val javaGetter = tpj.members.find(_.asTerm.name.decodedName.toString == getterName).getOrElse(
-        c.abort(c.enclosingPosition, s"Getter $getterName not found on ${tpj.typeSymbol.name}.")
+    def javaGetter(fieldName: String): (c.universe.TermName, c.universe.Type) = {
+      val getterName = s"get${fieldName.capitalize}"
+      val javaGetter = tpeJavaClass.members.find(_.asTerm.name.decodedName.toString == getterName).getOrElse(
+        c.abort(c.enclosingPosition, s"Getter $getterName not found on ${tpeJavaClass.typeSymbol.name}.")
       )
       val javaGetterName = javaGetter.asTerm.name
       val javaGetterType = javaGetter.typeSignature.resultType
+      (javaGetterName, javaGetterType)
+    }
 
-      (q"""javaObj.$javaSetterName(com.stackstate.scalajavamapper.Converter.toJava[$scalaFieldType, $javaSetterType](t.$name))""",
+    val companion = tpeCaseClass.typeSymbol.companion
+    val fields = tpeCaseClass.decls.collectFirst {
+      case m: MethodSymbol if m.isPrimaryConstructor ⇒ m
+    }.get.paramLists.head
+
+    val (toJavaStatements, fromJavaParams) = fields.map { field ⇒
+      val scalaFieldName = field.asTerm.name
+      val decodedFieldName = scalaFieldName.decodedName.toString
+      val scalaFieldType = tpeCaseClass.decl(scalaFieldName).typeSignature
+
+      val (javaSetterName, javaSetterType) = javaSetter(decodedFieldName)
+      val (javaGetterName, javaGetterType) = javaGetter(decodedFieldName)
+
+      (q"""javaObj.$javaSetterName(com.stackstate.scalajavamapper.Converter.toJava[$scalaFieldType, $javaSetterType](t.$scalaFieldName))""",
         q"""com.stackstate.scalajavamapper.Converter.fromJava[$scalaFieldType, $javaGetterType](j.$javaGetterName)""")
     }.unzip
 
@@ -44,14 +53,14 @@ object ConverterMacro {
 
     c.Expr[Converter[T, J]] {
       q"""
-  new Converter[$tpe, $tpj] {
-    def write(t: $tpe): $tpj = {
-      val javaObj = new $tpj()
+  new Converter[$tpeCaseClass, $tpeJavaClass] {
+    def write(t: $tpeCaseClass): $tpeJavaClass = {
+      val javaObj = new $tpeJavaClass()
       $setJavaFields
       javaObj
     }
 
-    def read(j: $tpj): $tpe = $companion(..$fromJavaParams)
+    def read(j: $tpeJavaClass): $tpeCaseClass = $companion(..$fromJavaParams)
   }
   """
     }
